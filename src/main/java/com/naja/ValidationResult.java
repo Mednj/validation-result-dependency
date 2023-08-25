203997f3a122b2f6596084e5e302862bc78cf134
Med Nj <razerhazer.2020@gmail.com>
2023-08-25 12:53:30 +0100

    testing repo

---
+++ src/main/java/com/naja/ValidationResult.java
@@ -0,0 +1,35 @@
+package com.naja;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class ValidationResult {
+    private List<String> errors = new ArrayList<>();
+    private String errorReasonCode;
+    private String errorReasonDescription;
+
+    private String validMessage;
+
+
+    public void addError(String error) {
+        errors.add(error);
+    }
+
+    public boolean isValid() {
+        return errors.isEmpty();
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+}
